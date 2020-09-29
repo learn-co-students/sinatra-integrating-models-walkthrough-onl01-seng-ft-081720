6530407947c2afd7622fded466fdc388251cce3a
SophiaGrace16 <sophia.g.white@outlook.com>
2020-09-29 16:41:53 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
+require 'pry'
 
 class App < Sinatra::Base
   get '/' do
@@ -8,7 +9,7 @@ class App < Sinatra::Base
 
   post '/' do
     @analyzed_text = TextAnalyzer.new(params[:user_text])
-    
+    # binding.pry
     erb :results
   end
 end
